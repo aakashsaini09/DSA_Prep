bc2bc214e526b833d42143b26d2fa3c9060dd748
aakashsaini09 <aakashsaini948585@gmail.com>
2023-07-26 13:34:18 +0530

    adding deploy yml file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
-    base: '/',
+    base: '/todo/'
   }
 
   if (command !== 'serve') {
