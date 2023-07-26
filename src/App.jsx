febf39997b060729ce2a07e64788253d0b18f02a
aakashsaini09 <aakashsaini948585@gmail.com>
2023-07-26 13:25:19 +0530

    adding complete project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Todo from './todocomp/Todo'
+
+
+function App() {
+  return (
+    <>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/todo/'
+  }
+
+  return config
+})
