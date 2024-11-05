f99fdd83784925c623d528758c1117ff52304a05
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-05 16:49:41 +0530

    config midware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 import express from 'express'
-import { loginUser } from './src/routes/auth';
+import { loginUser } from './src/routes/auth.js';
+import dotenv from 'dotenv'
 const app = express()
-const PORT = process.env.PORT;
-
+const PORT = 4000;
+dotenv.config()
 
 app.post('/api/auth/user', loginUser)
 
 
 app.listen(PORT, () => {
+    // const dbConnect = mongoose.connect("mongodb+srv://aakashsaini948585:jXYSp8aOVcuYZoEB@cluster0.abmbj.mongodb.net/")
+    // if(dbConnect){
+    //     console.log("db Connected")
+    // }else{
+    //     console.log("db not Connected")
+    // }
     console.log(`Server is running on ${PORT} port`);
 })
