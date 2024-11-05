5c27ff95122f3d2058719c30b125b14d6b6f017d
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-05 16:31:36 +0530

    db setup

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,4 +1,7 @@
+import mongoose from "mongoose";
+
 export const loginUser = (req, res) => {
     const {email, password} = req.body;
     console.log(email, password);
+    mongoose.connect(process.env)
 }
