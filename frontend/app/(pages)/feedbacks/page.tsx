5049fa6215c5224f13b3eceabf1e2ea3728df4ec
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-05 16:11:43 +0530

    toaster

--- frontend/app/(pages)/feedbacks/page.tsx
+++ frontend/app/(pages)/feedbacks/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { Switch } from '@/components/ui/switch'
 import { Label } from "@/components/ui/label"
 import { FiRefreshCw } from "react-icons/fi";
+import { FaCopy } from "react-icons/fa";
+import { useToast } from "@/hooks/use-toast"
 import React from 'react'
 const page = () => {
+    const {toast} = useToast()
     const userdata = [
         {
             title: 'Your outfit was amazing today.',
@@ -26,10 +30,15 @@ const page = () => {
             time: 'Feb 12, 2003 02:44 PM'
         },
     ]
+    const copyFunction = () =>{
+        let value = 'https://yoururl-1de6d-242.com';
+        navigator.clipboard.writeText(value);
+        toast({description: "URL copied!!" })
+    }
   return (
     <>
-      <div className="min-h-[100vh] min-w-[100vw] bg-black">
-        <nav>
+      <div className="min-h-[100vh] max-w-[100vw] bg-black overflow-hidden">
+        <nav className='w-[100vw]'>
             <ul className='w-[100vw] py-5 bg-gray-600 flex justify-between items-center px-8'>
             <li className='text-4xl font-bold text-white'>Logo</li>
                 <li className='text-xl font-bold text-gray-200'>Welcome Aakash Saini</li>
@@ -39,11 +48,11 @@ const page = () => {
         <div className="link px-44 pt-10">
             <h1 className='text-5xl text-white font-extrabold py-2'>User Dashboard</h1>
             <p className='text-base text-gray-300'>Copy Your Unique URL and Share with friends</p>
-            <div className="w-full flex justify-between items-center">
+            <div className="w-full flex justify-between items-center bg-gray-950 rounded-lg pr-4">
                 <div className="text-white underline px-2 py-3 rounded-sm text-xl">https://yoururl-1de6d-242.com</div>
-                <Button className='text-lg' variant={'secondary'} size={'lg'}>Copy</Button>
+                <Button className='text-base' variant={'secondary'} onClick={copyFunction}>Copy <FaCopy /></Button>
             </div>
-            <div className='flex items-center '>
+            <div className='flex items-center pt-5'>
                 <Switch id='accept-msg'/>
                 <Label className='text-gray-300 pl-3' htmlFor="accept-msg">Accept Messages: ON</Label>
             </div>
