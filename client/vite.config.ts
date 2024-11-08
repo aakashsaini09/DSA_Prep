fcef424f6925c87bac81d57706e71ef8a2fdd58c
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-08 13:57:47 +0530

    adding vite

---
+++ client/vite.config.ts
@@ -0,0 +1,17 @@
+// @ts-ignore
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
+// npx shadcn@latest add toast
+// npx shadcn@latest add label
+// npx shadcn@latest add switch
--- frontend/app/(pages)/(auth)/login/page.tsx
+++ frontend/app/(pages)/(auth)/login/page.tsx
@@ -1,8 +1,18 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import axios from 'axios'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 const page = () => {
+  const [userData, setuserData] = useState({
+    email: '',
+    password: ''
+  })
+  const loginfunction = async() => {
+    const res = await axios.post('http://localhost:4000/api/auth/login', {email: userData.email, password: userData.password})
+    console.log("res is: ", res)
+  }
   return (
     <>
       <div className='min-h-[100vh] min-w-[100vw] bg-black flex justify-center items-center'>
@@ -12,17 +22,17 @@ const page = () => {
             <form>
               <div className="mb-4">
                 <label htmlFor="email" className="block mb-2 text-sm font-medium">Your email</label>
-                <input type="email" id="email" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4" placeholder="andrew@mail.com"/>
+                <input value={userData.email} onChange={(e) => setuserData({ ...userData, email: e.target.value })}  type="email" id="email" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4" placeholder="andrew@mail.com"/>
               </div>
               <div className="mb-4">
                 <label htmlFor="password" className="block mb-2 text-sm font-medium">Your password</label>
-                <input type="password" id="password" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"placeholder="*********" />
+                <input value={userData.password} onChange={(e) => setuserData({ ...userData, password: e.target.value })}  type="password" id="password" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"placeholder="*********" />
               </div>
               <div className="flex items-center justify-between mb-4">
                 <button type="submit" className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 w-full sm:w-auto" > Submit </button>
                 <div className="flex items-center text-sm">
                   <p>New here?</p>
-                  <Link href='/signup' className="underline cursor-pointer ml-1 hover:text-blue-500">Register</Link>
+                  <Button onClick={loginfunction} className="underline cursor-pointer ml-1 hover:text-blue-500">Register</Button>
                 </div>
               </div>
             </form>
