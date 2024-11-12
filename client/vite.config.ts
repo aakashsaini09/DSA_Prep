d53693f72e8feb0d8b2a78c1b3006eb1e6172a26
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-12 19:01:44 +0530

    bug fix

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react(), envCompatible()],
-  envPrefix: "REACT_APP_",
+  envPrefix: "VITE_APP_",
   resolve: {
     alias: {
       // @ts-ignore
