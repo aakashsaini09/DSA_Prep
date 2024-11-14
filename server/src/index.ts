b74eb471275cdd8f6a949ba3d174ad3687e22e63
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-14 15:31:07 +0530

    login/signup done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { decode, verify, sign } from 'hono/jwt';
 // const app = new Hono()
 // ************************************auth*********************************
 const app = new Hono<{
@@ -9,15 +10,66 @@ const app = new Hono<{
     JWT_SEC: string
 	}
 }>();
-app.post('/api/auth/login', (c) => {
+
+
+app.post('/api/auth/login', async(c) => {
+  const body = await c.req.json()
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
 }).$extends(withAccelerate())
-  return c.text('Hello Hono!')
+try {
+  const user = await prisma.user.findFirst({
+    where:{
+      email: body.email,
+      password: body.password
+    }
+  })
+  if(!user){
+    c.status(403);
+    return c.json({
+      message: "Incorrect Credentials"
+    })
+  }
+  const jwt = await sign({
+    id: user.id
+  }, c.env.JWT_SEC)
+  return c.json({
+    message: "signin Success",
+    user: user
+  })
+} catch (err) {
+  c.status(411);
+  return c.text("Invalid Credentials")
+}
 })
-app.post('/api/auth/signup', (c) => {
-  return c.text('Hello Hono!')
+
+
+app.post('/api/auth/signup', async(c) => {
+  const body = await c.req.json()
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+try {
+  const user = await prisma.user.create({
+    data:{
+      name: body.name,
+      email: body.email,
+      accepting: true,
+      password: body.password
+    }
+  })
+  const jwt = await sign({
+    id: user.id
+  }, c.env.JWT_SEC)
+  return c.text('Sign-up Successfully!')
+} catch (err) {
+  c.status(411);
+  return c.text("Invalid Credentials")
+}
 })
+
+
+
 app.get('/api/auth/getall', (c) => {
   return c.text('Hello Hono!')
 })
